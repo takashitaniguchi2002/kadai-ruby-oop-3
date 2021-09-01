f636d7379edcf062b2ebc8a14cf7d3abaac4be6b
takashi.taniguchi <tks2002@icloud.com>
2021-09-01 02:04:05 +0000

    first commit

---
+++ human.rb
@@ -0,0 +1,14 @@
+require './animal'
+class Human < Animal
+ 
+    require './thinkable'
+    include Thinkable
+    
+    attr_accessor:hobby
+    
+    def initialize(name,age,hobby)
+    self.name = name
+    self.age = age
+    self.hobby = hobby
+    end
+end
